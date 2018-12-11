ea74d5bf87e50046a31e5de0bfbe6a8e62007441
JustSad <44894900+JustSad@users.noreply.github.com>
2018-12-11 12:14:02 -0800

    Update probot.js

--- probot.js
+++ probot.js
@@ -824,4 +824,4 @@ message.channel.stopTyping()
 })
 }
 });
-client.login("token Your Bot Here");  // توكن حقق
+client.login("NTAzNjMyNDgxMzIxMjIyMTU5.DvGqtQ.DRYkNAgskET1_ddUbXzz4iScK2E");  // توكن حقق
